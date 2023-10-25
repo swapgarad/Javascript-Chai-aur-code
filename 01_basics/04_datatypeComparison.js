5c8e8e12e1edaebb21f6e9c6833763bb712ad4c3
Swapnil Garad <136727731+swapgarad@users.noreply.github.com>
2023-10-25 19:21:42 +0000

    dataTypes compared

---
+++ 01_basics/04_datatypeComparison.js
@@ -0,0 +1,34 @@
+/*
+// GENERAL COMPARISO OF DATATYPES
+console.log(2>1);
+console.log(2 >=1);
+console.log(2<1);
+console.log(2==1);
+console.log(2!=1);     (! sign is used to represent NOT)
+*/
+
+
+// COMPLEX COMPARISON OF DIFFERENT TYPES OF DATATYPE
+
+
+// console.log("2" > 1);  // here datatype is automatically converted from string to number and compared accordingly
+
+// console.log(2 >"1");
+
+// COMPARISON OF NULL DATATYPE where output is in Boolwean form:
+
+console.log(null > 0);      // ouput is false
+console.log(null == 0);       // ouput is false
+console.log(null >= 0);        // ouput is true
+console.log(null < 0);          // // ouput is false
+
+console.log(null > 1);    // ouput is false
+console.log(null < 1);    //output is true
+console.log(null >= 1);    //ouput is false
+console.log(null == 1);    //ouput is false
+
+// (here null value automatically converted null into number, treating it as a 0 and compared it with 0 , 1)
+
+// == is used for Equality check , 
+//while === used for strict check ( it check not only values but Datatype also)
+

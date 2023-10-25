4d58e4e5f3a570cfa3ec8301584f64995d4e5e53
Swapnil Garad <136727731+swapgarad@users.noreply.github.com>
2023-10-25 08:48:55 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict;"      //it used to treat all JS code as newer version of javascript
+
+// alert(3+3)  // it doesn't shown in git/vs code as we are using nodeJS & not a browser
+
+// code readibility should be high
+
+/*
+DATATYPES : 1) PREMITIVE DATATYPES
+number => 2 TO POWER  OF 53
+bigint => used in bigger sites where large numbers are involved like share market, fb, reddit
+string => used to write names/words... can used in both '' & " "
+boolean => True/False
+null => standalone value....representation of empty value...its type of object
+undefined => variable is declares but value is not given/decided yet..
+symbol => used specify uniqueness of value...generally mostly used in React
+
+
+2) 
+object => 
+
+*/
+
+let name="swapnil"
+let age=30
+
+console.log(typeof "swapnil");
+console.log(typeof age);
+
+console.log(typeof null); // type of null came as OBJECT
+console.log(typeof undefined); // type of undefined came as undefined itself

22771cb4347a58e9f93536e5ce45a668536b1605
Swapnil Garad <136727731+swapgarad@users.noreply.github.com>
2023-10-25 11:16:49 +0000

    Conversion of DataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,35 @@
+//****************CONVERSION************//
+
+let score = "swapnil"
+
+//console.log(typeof score);
+//console.log(typeof (score));
+
+let valueInNumber = Number(score);
+
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "45" =>output= 45; typeof data=number 
+// "45abc" =>output= NaN i.e. Not a Number; typeof data=number 
+// true =>output=1 ; typeof data=boolean
+// false =>output=0 ; typeof data=boolean
+
+let isLoggedIn = "swapnil"
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+/*
+ for boolean value conversion:
+ 1 => true ; 0=> false
+ " " => false
+ "swapnil" => true
+ */
+
+ let someNumber = 10
+
+ let stringNumber = String(someNumber)
+
+ console.log(stringNumber);
+ console.log(typeof stringNumber);

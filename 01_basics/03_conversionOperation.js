026c923e890ed76feec5ecaefbf7aa1ca049bf53
Swapnil Garad <136727731+swapgarad@users.noreply.github.com>
2023-10-25 14:17:57 +0000

    operations discussed

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -7,8 +7,8 @@ let score = "swapnil"
 
 let valueInNumber = Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 
 // "45" =>output= 45; typeof data=number 
@@ -18,7 +18,7 @@ console.log(valueInNumber);
 
 let isLoggedIn = "swapnil"
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 /*
  for boolean value conversion:
@@ -31,5 +31,39 @@ console.log(booleanIsLoggedIn);
 
  let stringNumber = String(someNumber)
 
- console.log(stringNumber);
- console.log(typeof stringNumber);
+//  console.log(stringNumber);
+//  console.log(typeof stringNumber);
+
+ //*************OPERTIONS**********
+
+let value = 5
+let negValue = -value
+// console.log(-value);
+
+
+// Joining two string:
+let str1 = "hello"
+let str2 = " Swapnil"
+
+let str3 = (str1 + str2)
+
+// console.log(str3);
+
+// Joining complex/different datatype string:
+
+// console.log("1" + 2);   // output=12
+// console.log(1 + "5");   //output=15
+// console.log(1 + "5" + 9);   //output=159
+// console.log(1 + 5 + "9");   //output=69
+
+console.log(+true);   // output will be 1(as 0 + 1=1)
+console.log(+"");     // output will be 0(as 0+0=0)
+
+// console.log(true+);   // output will be error as operator precedent is wrong
+
+// PREFIX & POSTFIX INCREMENT OPERATOR
+
+let gameCounter = 450
+gameCounter++
+++gameCounter
+console.log(gameCounter);

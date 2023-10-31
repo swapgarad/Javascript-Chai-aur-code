d75fefd57b67fac3bb9a8a04ea84f886ee41c1f5
Swapnil Garad <136727731+swapgarad@users.noreply.github.com>
2023-10-31 12:28:30 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+// Traditional method of coding string:(its old method which rarely used nowadays.)
+
+const name = "swapnil"
+const repoCount = "01"
+
+// console.log(name + repoCount + " value"); ====>>> its traditional outdated method not used currently....instead of that we use "STRING INTERPOLATION METHOD"...it create placeholders & we can directly inject variable as per our requirement in ${ }.
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount}`);
+
+// it gives output as ===>>> Hello my name is swapnil and my repocount is 01
+
